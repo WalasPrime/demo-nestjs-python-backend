d974fb920f40da3148e5e3d6961192585e03f4fd
Karol Walasek <coreconviction@gmail.com>
2026-08-05 13:58:33 +0200

    chore(api): introduce jest-mock-extended along with a simple dispatcher mock and fix tests

--- apps/api/src/app.controller.spec.ts
+++ apps/api/src/app.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DispatcherService } from './modules/dispatcher/dispatcher.service';
+import { dispatcherServiceMock } from './modules/dispatcher/dispatcher.mock';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,15 +10,21 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: DispatcherService,
+          useValue: dispatcherServiceMock,
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World!"', async () => {
+      await expect(appController.getHello()).resolves.toBe('Hello World!');
     });
   });
 });
---
+++ apps/api/src/modules/dispatcher/dispatcher.mock.ts
@@ -0,0 +1,4 @@
+import { mockDeep } from 'jest-mock-extended';
+import { DispatcherService } from './dispatcher.service';
+
+export const dispatcherServiceMock = mockDeep<DispatcherService>();
